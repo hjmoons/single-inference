c764cb201d76c365ac3d5560ac8efd2b7fb94581
HyoJong Moon <gywhd408@naver.com>
2020-04-14 16:09:16 +0900

    fix arguments to execute model in ModelExecutorMain

--- src/main/java/dke/executor/ModelExecutorMain.java
+++ src/main/java/dke/executor/ModelExecutorMain.java
@@ -4,10 +4,10 @@ import dke.executor.model.DataPipeline;
 
 public class ModelExecutorMain {
     public static void main(String[] args) {
-        String bootstrap = "114.70.235.43:19092,114.70.235.43:19093,114.70.235.43:19094,114.70.235.43:19095,114.70.235.43:19096,114.70.235.43:19097,114.70.235.43:19098,114.70.235.43:19099,114.70.235.43:19100";
-        String inputTopic = "mnist-input";
-        String outputTopic = "mnist-output";
-        String servingAPI = "http://114.70.235.43:8501/v1/models/mnist:predict";
+        String bootstrap = "MN:49092,SN01:49092,SN02:49092,SN03:49092,SN04:49092,SN05:49092,SN06:49092,SN07:49092,SN08:49092";
+        String inputTopic = args[0];
+        String outputTopic = args[1];
+        String servingAPI = args[2];
 
         DataPipeline inputConsumer = new DataPipeline(bootstrap, inputTopic, outputTopic).InputConsumer(servingAPI);
         inputConsumer.consume();
