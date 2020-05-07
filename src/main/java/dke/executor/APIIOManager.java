9bc8ed252c9b9ea012bdfa6f5be9113e30538e27
HyoJong-Moon <gywhd408@naver.com>
2020-05-07 11:28:57 +0900

    delete content of bootstrap variable in APIIOManager

--- src/main/java/dke/executor/APIIOManager.java
+++ src/main/java/dke/executor/APIIOManager.java
@@ -4,7 +4,7 @@ import dke.executor.model.APIExecutor;
 
 public class APIIOManager {
     public static void main(String[] args) {
-        String bootstrap = "MN:49092,SN01:49092,SN02:49092,SN03:49092,SN04:49092,SN05:49092,SN06:49092,SN07:49092,SN08:49092";
+        String bootstrap = "";
         String inputTopic = args[0];
         String outputTopic = args[1];
         String servingAPI = args[2];
